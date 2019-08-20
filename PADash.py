1415656227e4810ce569d3ba9c6d0abaaced9b03
fannyaqmarina <aqmarinanas@gmail.com>
2019-08-20 10:22:27 +0800

    Add files via upload

--- PADash.py
+++ PADash.py
@@ -12,27 +12,84 @@ import scipy.stats as stats
 import numpy as np
 
 
+df1 = pd.read_csv(r"C:\Users\ASPIRE E 14\Music\DTS FGA 2019 - Unmul\projek akhir\ProjekAkhir\diabetes.csv")
+data = df1['Diabetic'].value_counts()
+
 logistik = 'Logistik.png'
 KNN = 'KNN.png'
+knnNotNorm = 'KNNnotNorm.png'
+KNNNorm='KNNNorm.png'
+Histogram = 'Histogram.png'
 test_logistik = base64.b64encode(open(logistik, 'rb').read()).decode('ascii')
+test_histogram = base64.b64encode(open(Histogram, 'rb').read()).decode('ascii')
 test_Knn = base64.b64encode(open(KNN, 'rb').read()).decode('ascii')
-
+test_Knnnotnorm = base64.b64encode(open(knnNotNorm, 'rb').read()).decode('ascii')
+test_Knnnorm = base64.b64encode(open(KNNNorm, 'rb').read()).decode('ascii')
 
 #DASH APP
 app = dash.Dash()
-
-app.layout = html.Div(children=[
-    html.H2('Regresi Logistik',style={
-            'textAlgin':'center'}),
-    html.Br(),
-    html.Img(src='data:image/png;base64,{}'.format(test_logistik),style={
-            'textAlgin':'center'}),
-    html.Br(),
-     html.Br(),
-    html.H2('K-Nearest Neighbor',style={
-            'textAlgin':'center'}),
+colors = {
+    'background': '#ffffff',
+    'text': '#ff6977'
+}
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Dash Kelompok 5',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+    html.Div(children='Klasifikasi Penyakit Diabetes Berdasarkan Tindakan Diagnostik', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+     dcc.Graph(
+        id='Graph1',
+        figure={
+            'data': [
+                {'x': [0,1], 'y': [data[0],data[1]], 'type': 'bar'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+      html.H2('Histogram',style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+    html.Img(src='data:image/png;base64,{}'.format(test_histogram),style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+     html.H2('K-Nearest Neighbor Sebelum Normalisasi',style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+    html.Img(src='data:image/png;base64,{}'.format(test_Knnnotnorm),style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+     html.H2('K-Nearest Neighbor Setelah Normalisasi',style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+    html.Img(src='data:image/png;base64,{}'.format(test_Knnnorm),style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+     html.H2('K-Nearest Neighbor Akurasi',style={
+            'textAlign': 'center',
+            'color': colors['text']}),
     html.Img(src='data:image/png;base64,{}'.format(test_Knn),style={
-            'textAlgin':'center'}),
+            'textAlign': 'center',
+            'color': colors['text']}),
+    html.H2('Regresi Logistik',style={
+            'textAlign': 'center',
+            'color': colors['text']}),
+    html.Img(src='data:image/png;base64,{}'.format(test_logistik),style={
+            'Align': 'center',
+            'color': colors['text']}),
+   
 ])
             
 if __name__ == '__main__':
